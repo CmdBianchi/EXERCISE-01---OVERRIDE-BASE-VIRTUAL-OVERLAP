601086dd148f32894b010354f9344f95eb5baecf
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-10 17:31:43 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 01 - OVERRIDE BASE  VIRTUAL OVERLAP/ENTITIES/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//------------------------------- START -------------------------------//
+namespace EXERCISE_01___OVERRIDE_BASE__VIRTUAL_OVERLAP.ENTITIES  {
+    //------------------------------- START -------------------------------//
+    class Account {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+    //------------------------------- CONST -------------------------------//  
+        public Account() { }
+
+        public Account(int number, string holder, double balance) {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+    //------------------------------- MET --------------------------------//
+        public virtual void Withdraw(double amount) {
+            Balance -= amount + 5;
+        }
+        public void Deposit(double amount) {
+            Balance += amount;
+        }
+    }
+//-------------------------------- END -------------------------------//
+}
---
+++ EXERCISE 01 - OVERRIDE BASE  VIRTUAL OVERLAP/ENTITIES/SavingsAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EXERCISE_01___OVERRIDE_BASE__VIRTUAL_OVERLAP.ENTITIES {
+    //------------------------------- START -------------------------------//
+    class SavingsAccount : Account {
+        public double Interesrate { get; set; }
+    //------------------------------- CONST -------------------------------//  
+        public SavingsAccount() {}
+
+        public SavingsAccount(int number, string holder, double balance, double interesrate) : base(number, holder, balance) {
+            Interesrate = interesrate;
+        }
+    //------------------------------- MET --------------------------------//
+        public void UpdateBalance() {
+            Balance += Balance * Interesrate;
+        }
+        public override void Withdraw(double amount) {
+            Balance -= amount;
+        }
+
+    }
+    //-------------------------------- END -------------------------------//
+}
